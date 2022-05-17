9c50c1276a8aa720fdf30ef216b7eae8cf7b2126
Andrii <example@gmail.com>
2022-05-17 19:51:59 +0300

    geometry

--- js/script.js
+++ js/script.js
@@ -30,69 +30,88 @@
 
 // Место для первой задачи
 function firstTask() {
-    // Пишем решение вот тут
-    for (let i = 5; i <= 10; i++) {
-        console.log(i);
-    }
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
 
+    // Пишем решение вот тут
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+    // Не трогаем
+    return result;
 }
 
 // Место для второй задачи
 function secondTask() {
-    // Пишем решение вот тут
-    for (let i = 20; i > 9; i--) {
-        if (i === 13) break;
-        console.log(i);
-    }
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
 
+    // Пишем решение вот тут
+    for (let i = 0; i < data.length; i++) {
+        if (typeof data[i] === "number") {
+            data[i] *= 2;
+        } else if (typeof data[i] === "string") {
+            data[i] += ' - done';
+        }
+    }
+    console.log(data);
+    // Не трогаем
+    return data;
 }
 
 // Место для третьей задачи
 function thirdTask() {
-    // Пишем решение вот тут
-    for (let i = 2; i <= 10; i += 2) {
-        console.log(i);
-    }
-
-}
-
-// Место для четвертой задачи
-
-// Цикл, который нужно переписать:
-
-// for (let i = 2; i <= 16; i++) {
-//     if (i % 2 === 0) {
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-// }
-
-function fourthTask() {
-    // Пишем решение вот тут
-    let i = 2;
-    while (i < 16) {
-        i++;
-        if (i % 2 === 0) {
-            continue;
-        } else {
-            console.log(i);
-        }
-    }
-}
-
-// Место для пятой задачи
-
-function fifthTask() {
-    const arrayOfNumbers = [];
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
 
     // Пишем решение вот тут
     let j = 0;
-    for (let i = 5; i <= 10; i++) {
-
-        arrayOfNumbers[j] = i;
+    for (let i = data.length - 1; i >= 0; i--) {
+        result[j] = data[i];
         j++;
     }
+    console.log(result);
+
     // Не трогаем
-    return arrayOfNumbers;
+    return result;
 }
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) { // main cycle going throught lines of figure
+    for (let j = lines; j >= 0; j--) { // making spaces , with each i cycle spacing is getting smaller
+        result += ' ';
+        if (j === i) { // when j matches i starting to draw *
+            for (let k = 0; k <= i + i; k++) { // drawing * 
+                result += '*';
+            }
+        }
+    }
+    result += '\n';
+}
+console.log(result);
+
+//      *
+//     *** 
+//    *****  
+//   *******   
+//  *********    
+// ***********  
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+
+console.log(result)
