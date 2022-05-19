4d691c4bce4605e4f42c181d81e9decc22e20d04
Andrii <example@gmail.com>
2022-05-19 15:09:07 +0300

    added file with console programm

---
+++ script1.js
@@ -0,0 +1,88 @@
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+"use strict";
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How many films have you seen ?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films have you seen ?', '');
+    }
+}
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const numberOfAskedQuestions = 2;
+
+function rememberMyFilms() {
+    for (let i = 0; i < numberOfAskedQuestions; i++) {
+        const lastWatchedMovie = prompt('One of the last watched movies ?', '').trim();
+        const rateMovie = prompt('Give rating to that movie ?', '');
+
+        if (lastWatchedMovie != null && rateMovie != null && lastWatchedMovie != '' &&
+            rateMovie != '' && lastWatchedMovie.length < 50) {
+            personalMovieDB.movies[lastWatchedMovie] = rateMovie;
+            console.log("success");
+        } else {
+            console.log('Error');
+            i--;
+        }
+    }
+}
+
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Переглянуто доволі мало фільмів');
+    } else if ((personalMovieDB.count >= 10) && (personalMovieDB.count < 30)) {
+        console.log('Ви класичний глядач');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Ви кіноман');
+    } else {
+        console.log('Сталася помилка');
+    }
+}
+
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+
+    for (let i = 0; i < 3; i++) {
+        const favouriteGenre = prompt(`Your favorite genre number ${i + 1}`, '');
+        personalMovieDB.genres[i] = favouriteGenre;
+    }
+}
+
+writeYourGenres();
+
+
+// console.log(personalMovieDB);
